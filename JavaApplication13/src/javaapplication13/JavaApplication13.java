5b9fc740bc826c0d0c859edfc2973946d35e2e65
Profesor <Profesor@INF700>
2013-02-11 14:09:02 +0100

    nueva modificación

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -16,6 +16,7 @@ public class JavaApplication13 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Espero que funcione");
+        System.out.println("Nueva modificación");
         
     }
 }
